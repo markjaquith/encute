3c2e4215ee6f2e168e443a68c72efd367def389f
Mark Jaquith <mark@jaquith.me>
2021-10-09 16:28:08 -0400

    Remove the ExampleProvider

--- app/Plugin.php
+++ app/Plugin.php
@@ -7,7 +7,6 @@ class Plugin extends Tekta\Plugin {
 	 * List of Provider class names that extend CWS\Encute\Illuminate\Support\ServiceProvider
 	 */
 	protected $providers = [
-		Providers\ExampleProvider::class,
 		Tekta\Providers\TranslationsProvider::class,
 	];
 }
