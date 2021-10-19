960f8dce435a1c7182c02aed41a16eef074fe8a0
Mark Jaquith <mark@jaquith.me>
2021-10-19 15:44:24 -0400

    Add Mozart PSR files

---
+++ lib/Mozart/packages/Psr/Container/NotFoundExceptionInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace CWS\Encute\Mozart\Psr\Container;
+
+/**
+ * No entry was found in the container.
+ */
+interface NotFoundExceptionInterface extends ContainerExceptionInterface
+{
+}
