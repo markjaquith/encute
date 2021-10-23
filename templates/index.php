90bd3ee4b29259f39959e293774fc295d16a8706
Mark Jaquith <mark@jaquith.me>
2021-10-23 13:39:59 -0400

    v0.8.2 fix h1

--- templates/index.php
+++ templates/index.php
@@ -24,7 +24,7 @@
 </style>
 
 <div class="wrap encute">
-	<h1><?php esc_html($title) ?></h1>
+	<h1><?php echo esc_html($title) ?></h1>
 	<div class="encute-card">
 		<h2 class="title">Getting Started</h2>
 		<p><b>Encute</b> allows you to manipulate the enqueued scripts and styles on the front of your site.</p>
