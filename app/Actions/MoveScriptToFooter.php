54e45073fd3d99a888c1dc8b289f521e78798c16
Mark Jaquith <mark@jaquith.me>
2021-10-26 15:56:36 -0400

    Use same method for moving scripts as styles, to preserve info better

--- app/Actions/MoveScriptToFooter.php
+++ app/Actions/MoveScriptToFooter.php
@@ -3,19 +3,13 @@
 namespace CWS\Encute\Actions;
 
 class MoveScriptToFooter extends Action {
-	protected function move(\WP_Scripts $wpScripts, string $handle) {
-		if (wp_script_is($handle, 'registered')) {
-			$src = $wpScripts->registered[$handle]->src;
-			$version = $wpScripts->registered[$handle]->ver;
-			$dependencies = $wpScripts->registered[$handle]->deps;
-			wp_deregister_script($handle);
-			wp_register_script($handle, $src, $dependencies, $version, true);
-		}
-	}
-
 	public function handle(\WP_Scripts $wpScripts): void {
 		foreach ($this->asset->getHandles() as $handle) {
-			$this->move($wpScripts, $handle);
+			if (wp_script_is($handle, 'registered')) {
+				$script = $wpScripts->registered[$handle];
+				wp_deregister_script($handle);
+				add_action('wp_head', fn() => $wpScripts->registered[$handle] = $script, PHP_INT_MAX);
+			}
 		}
 	}
 }
